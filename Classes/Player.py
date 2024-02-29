e6453dbae435a864ec39c84607d37a18c66a6bef
Honuratus <onurbaranyilmaz@gmail.com>
2024-02-29 19:05:55 +0300

    Update Player.py

---
+++ Classes/Player.py
@@ -0,0 +1,23 @@
+from Deck import Deck
+
+
+class Player():
+    def __init__(self, username):
+        self.stash = list()
+        self.hand = list()
+        self.username = username
+        self.total_points = 0
+
+    def play_card(self, idx):
+        temp = self.hand[idx]
+        self.hand.remove(temp)
+        return temp
+
+    def pot_to_stash(self, pot):
+        # pot bir list append olunacak
+        for card in pot:
+            self.stash.append(card)
+            pot.pop()
+
+        
+        
